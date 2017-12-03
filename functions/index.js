8ff51272038eb539860bfff21e75c3b66f8c198c
Charles Thomas <CharlesThomas100@gmail.com>
2017-12-03 11:29:15 -0800

    starting setup of API endpoints

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,23 @@
 const functions = require('firebase-functions');
+const firebase = require('firebase-admin');
+const express = require('express');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+const firebaseApp = firebase.initialize(
+    functions.config().firebase
+);
+
+const app = express();
+app.get('/df', (request, response) =>{
+    response.send(`${Date.now()}`);
+});
+
+app.get('slk', (request, response) =>{
+    response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+    repsonse.send(`${Date.now()}`);
+});
+
+exports.app = functions.https.onRequest(app);
+
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
