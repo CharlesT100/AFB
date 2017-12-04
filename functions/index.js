23f951c317774b64269453c97f0253396e594c07
Charles Thomas <CharlesThomas100@gmail.com>
2017-12-04 14:57:11 -0800

    request timeout

--- functions/index.js
+++ functions/index.js
@@ -1,25 +1,29 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
-
-const express = require('express');
 let serviceAccount = require("./serviceAccountKey.json");
 
-const app = express();
-app.post('/dfpostmsg', (request, response) =>{
+// const express = require('express');
 
-// function that retreives the text sent and adds
+// const app = express();
+// app.post('/dfpostmsg', (request, response) =>{
+//     response.send('Hello From Firebase');
+// });
+
+// app.get('/dfpostqs', (request, response) =>{
+//     response.send('Hello From Firebase');
+// });
+
+// app.get('/slk', (request, response) =>{
+//     response.send('Hello From Firebase');
+// });
+// exports.app = functions.https.onRequest(app);
+
+exports.createDdfTest = functions.https.onRequest((req, res) => {
+  response.send("hello y'all");
 });
 
-app.get('/dfpostqs', (request, response) =>{
-    response.send('Hello From Firebase');
-});
 
-app.get('/slk', (request, response) =>{
-    response.send('Hello From Firebase');
-});
-
-exports.app = functions.https.onRequest(app);
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((req, res) => {
     
@@ -30,7 +34,7 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((req, res) => 
     const parameters = req.body.queryResult.parameters; 
     const inputContexts = req.body.queryResult.contexts;
     const requestSource = (req.body.originalRequest) ? req.body.originalRequest.source : undefined;
-    
+
     // Firestore database
     const db = admin.firestore()
     const actionHandlers = {
@@ -44,6 +48,73 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((req, res) => 
                 res.json(data)
             })
         },
+
+        'add.chatbotIntent': () => {
+
+            // const question = parameters['question'];
+            // const answer = parameters['answer'];
+            // const example = parameters['code-example'];
+
+            //Make call to DialogFlow SDK
+            function createIntents(projectId, req){
+
+                const dialogFlow = require('dialogflow');
+                const parameters = req.body.queryResult.parameters; 
+
+                const question = parameters['question'];
+                const answer = parameters['answer'];
+                const example = parameters['code-example'];
+                
+                 // Instantiates clients
+                 const contextsClient = new dialogflow.ContextsClient();
+                 const intentsClient = new dialogflow.IntentsClient();
+
+                 // The path to identify the agent that owns the created intent.
+                 const agentPath = intentsClient.projectAgentPath(projectId);
+
+                 // Set the question to be 'user says' within intent
+                 const afbPhrases = [
+                    {type: 'TYPE_EXAMPLE', parts: [{text: question }]},
+                  ];
+                  console.log(afbPhrases);
+
+                 // Setup the intent
+                 const afbIntent = {
+                    displayName: 'Dummy Intent',
+                    events: ['dummy_event'],
+                    // Webhook is disabled because we are not ready to call the webhook yet.
+                    webhookState: 'WEBHOOK_STATE_DISABLED',
+                    trainingPhrases: afbPhrases,
+                    mlEnabled: true,
+                    priority: 500000,
+                    result: answer,
+                  };
+                  console.log(afbIntent);
+
+                  // Associate intent to the parent/agentPath
+                  const afbRequest = {
+                    parent: agentPath,
+                    intent: afbIntent,
+                  };
+                  console.log(afbRequest);
+
+                  // Create the intent
+                    intentsClient
+                    .createIntent(afbRequest)
+                    .then(responses => {
+                        console.log('Created A New Intent:');
+                        logIntent(responses[0]);
+                    })
+                    .catch(err => {
+                        console.error('ERROR:', err);
+                    });
+            }
+            //Send response to Dialog Flow
+            const afbMessage = formatResponse('This message via Cloud Function and the add.chatbotIntent action');
+            console.log(afbMessage);
+            res.json(afbMessage)
+
+        }, // end of add.chatbotIntent
         'default': () => {
             const data = formatResponse('Hi. I am the default response from the Cloud Function')
         }
@@ -57,11 +128,54 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((req, res) => 
 
 function formatResponse(text) { // Helper to format the response JSON object
     return {
-        speech: text,
-        displayText: text,
-        data: {},
-        contextOut: [],
-        source: '',
-        followupEvent: {}
+        fulfillmentText: text,
+        // data: {},
+        // contextOut: [],
+        // source: '',
+        // followupEvent: {}
     }
 }
+
+// function formatResponse(text) {
+//     return {
+//         speech: text,
+//         displayText: text,
+//         data: {},
+//         contextOut: [],
+//         source: '',
+//         followupEvent: {}
+//     }
+// }
+
+// // Setup Param
+// const afbParameters = [
+// {
+//     displayName: 'question',
+//     value: question,
+//     entityTypeDisplayName: '@sys.text',
+// },
+// {
+//     displayName: 'answer',
+//     value: answer,
+//     entityTypeDisplayName: '@sys.text',
+//     isList: false,
+// },
+// ];
+
+// const afbResult = {
+// action: 'afb_confirm',
+// parameters: afbParameters,
+// messages: [
+//     {
+//     text: {
+//         text: [
+//         'We added the question: $question with the answer of: $answer.',
+//         ],
+//     },
+//     },
+// ],
+// // Conclude the conversation by setting no output contexts and setting
+// // resetContexts to true. This clears all existing contexts.
+// outputContexts: [],
+// resetContexts: true,
+// };
